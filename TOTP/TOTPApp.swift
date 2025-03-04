2104a71f964d34583d61f3ea585a3f735f6726d1
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2025-03-04 18:40:40 +0100

    Initial Commit

---
+++ TOTP/TOTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOTPApp.swift
+//  TOTP
+//
+//  Created by Lucas Fernández Aragón on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TOTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TOTPTests/TOTPTests.swift
@@ -0,0 +1,16 @@
+//
+//  TOTPTests.swift
+//  TOTPTests
+//
+//  Created by Lucas Fernández Aragón on 4/3/25.
+//
+
+import Testing
+
+struct TOTPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
